5c1a0374782f7ab621a861cba138cbe36d554708
dmordovskoy <7491683+dmordovskoy@users.noreply.github.com>
2026-08-02 19:56:16 -0400

    feat: add grounded jump

--- tests/player_test.gd
+++ tests/player_test.gd
@@ -6,6 +6,17 @@ func after_test() -> void:
 	Input.action_release("move_right")
 	Input.action_release("move_up")
 	Input.action_release("move_down")
+	Input.action_release("jump")
+
+
+func test_jump_action_uses_physical_space() -> void:
+	var has_physical_space := false
+	for event in InputMap.action_get_events("jump"):
+		var key_event := event as InputEventKey
+		if key_event != null and key_event.physical_keycode == KEY_SPACE:
+			has_physical_space = true
+			break
+	assert_bool(has_physical_space).is_true()
 
 
 func test_idle_without_input() -> void:
@@ -49,3 +60,56 @@ func test_turns_toward_movement_direction() -> void:
 	await runner.simulate_frames(90)
 	var player: Player = runner.scene()
 	assert_float(absf(angle_difference(player.rotation.y, PI / 2))).is_less(0.2)
+
+
+func test_jump_rises_lands_and_rejects_midair_impulse() -> void:
+	var runner := scene_runner("res://scenes/main/main.tscn")
+	await runner.simulate_frames(2)
+	var player: Player = runner.scene().get_node("Player")
+	_simulate_until_grounded(player)
+	assert_bool(player.is_on_floor()).is_true()
+	var floor_height := player.position.y
+
+	Input.action_press("jump")
+	player._physics_process(1.0 / 60.0)
+	Input.action_release("jump")
+	assert_float(player.position.y).is_greater(floor_height)
+	assert_float(player.velocity.y).is_equal_approx(player.jump_velocity, 0.001)
+
+	player._physics_process(1.0 / 60.0)
+	var velocity_before_second_press := player.velocity.y
+	Input.action_press("jump")
+	player._physics_process(1.0 / 60.0)
+	Input.action_release("jump")
+	assert_float(player.velocity.y).is_less(velocity_before_second_press)
+	assert_float(player.velocity.y).is_less(player.jump_velocity)
+
+	_simulate_until_grounded(player)
+	assert_bool(player.is_on_floor()).is_true()
+	assert_float(player.position.y).is_equal_approx(floor_height, 0.001)
+
+
+func test_horizontal_movement_unaffected_while_airborne() -> void:
+	var runner := scene_runner("res://scenes/main/main.tscn")
+	await runner.simulate_frames(2)
+	var player: Player = runner.scene().get_node("Player")
+	_simulate_until_grounded(player)
+	var starting_position := player.position
+
+	Input.action_press("jump")
+	player._physics_process(1.0 / 60.0)
+	Input.action_release("jump")
+	Input.action_press("move_right")
+	for _step in 10:
+		player._physics_process(1.0 / 60.0)
+
+	assert_float(player.position.x).is_greater(starting_position.x)
+	assert_float(player.position.y).is_greater(starting_position.y)
+	assert_float(player.velocity.x).is_greater(0.0)
+
+
+func _simulate_until_grounded(player: Player) -> void:
+	for _step in 120:
+		if player.is_on_floor():
+			return
+		player._physics_process(1.0 / 60.0)
