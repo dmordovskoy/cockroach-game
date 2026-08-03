1574ee91a1a6d6e957a9302cd781b5389348b3dd
dmordovskoy <7491683+dmordovskoy@users.noreply.github.com>
2026-08-02 23:37:19 -0400

    fix: keep version stamp unobtrusive at 4k

--- scripts/version_label.gd
+++ scripts/version_label.gd
@@ -5,3 +5,21 @@ extends CanvasLayer
 
 func _ready() -> void:
 	label.text = str(ProjectSettings.get_setting("application/config/version", "dev"))
+	get_viewport().size_changed.connect(_update_stamp_scale)
+	_update_stamp_scale()
+
+
+func apply_render_scale(render_size: Vector2, logical_size: Vector2) -> void:
+	if render_size.x <= 0.0 or render_size.y <= 0.0:
+		return
+	if logical_size.x <= 0.0 or logical_size.y <= 0.0:
+		return
+	var render_scale := minf(render_size.x / logical_size.x, render_size.y / logical_size.y)
+	label.pivot_offset = label.size
+	label.scale = Vector2.ONE / maxf(render_scale, 1.0)
+
+
+func _update_stamp_scale() -> void:
+	apply_render_scale(
+		Vector2(get_viewport().get_texture().get_size()), get_viewport().get_visible_rect().size
+	)
